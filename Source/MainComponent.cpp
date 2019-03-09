959e7d039916954054f81225f6013394b56ea049
Martin Dupras <martindupras@gmail.com>
2019-03-09 11:22:51 +0000

    First commit

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,42 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (600, 400);
+    slider1 = new Slider(("Slider 1"));
+    slider1->setBounds(90,90,200,200);
+    slider1->setRange(-10,1,0.01);
+    slider1->setSliderStyle(juce::Slider::Rotary);
+    addAndMakeVisible(slider1);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setFont (Font (16.0f));
+    g.setColour (Colours::white);
+    g.drawText ("Test to display a knob.", getLocalBounds(), Justification::centredRight, true);
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
